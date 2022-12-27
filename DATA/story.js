9eff9302242090fe18a32450d4770ed71ec462ea
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2022-12-27 08:38:08 +0300

    1

--- DATA/story.js
+++ DATA/story.js
@@ -1,60 +1,70 @@
 var data_story = [
     {
         "name": "AMD Ryzen 3 1200",
+        "type": "cpu",
         "price": [2400, 3600],
         "icon": "ryzen_3_1200.jpg",
         "link": "https://www.amd.com/en/products/cpu/amd-ryzen-3-1200"
     },
     {
         "name": "AMD Ryzen 3 1200",
+        "type": "cpu",
         "price": [2300, 3600],
         "icon": "ryzen_3_1200.jpg",
         "link": "https://www.amd.com/en/products/cpu/amd-ryzen-3-1200"
     },
     {
         "name": "GDP-650C A-90-SERIES",
+        "type": "psu",
         "price": [2500, 5399],
         "icon": "GDN_650C_A_90.png",
         "link": "https://www.chieftec.eu/products-detail/109/A-90-SERIES/111/GDP-650C"
     },
     {
         "name": "F4-3000C15Q-16GRR (EOL)",
+        "type": "ram",
         "price": [3500, 16900],
         "icon": "F4-3000C15Q-16GRR.webp",
         "link": "https://www.gskill.com/product/165/172/1535953960/F4-3000C15Q-16GRR-EOL"
     },
     {
         "name": "CUSO",
+        "type": "ram",
         "price": [3200, 0],
         "icon": "",
         "link": "https://aliexpress.ru/store/912510572?spm=a2g2w.detail.feedback.3.20375613aba3fg"
     },
     {
         "name": "B450M DS3H (rev. 1.x)",
-        "price": [3800, 0],
-        "icon": "",
+        "type": "mb",
+        "price": [3800, 6000],
+        "icon": "B450M_DS3H.webp",
         "link": "https://www.gigabyte.ru/products/page/mb/b450m_ds3hrev_10#kf"
     },
     {
+        "name": "PRIME B450M-K II",
+        "type": "mb",
+        "price": [4400, 8300],
+        "icon": "PRIME_B450M-K_II.webp",
+        "link": "https://www.asus.com/ru/motherboards-components/motherboards/prime/prime-b450m-k-ii/"
+    },
+    {
         "name": "",
+        "type": "",
         "price": [0, 0],
         "icon": "",
         "link": ""
     },
     {
         "name": "",
+        "type": "",
         "price": [0, 0],
         "icon": "",
         "link": ""
     },
     {
         "name": "",
-        "price": [0, 0],
-        "icon": "",
-        "link": ""
-    },
-    {
-        "name": "",
+        "type": "",
         "price": [0, 0],
         "icon": "",
         "link": ""
--- DATA/venders.js
+++ DATA/venders.js
@@ -30,6 +30,76 @@ var venders_data = [
         "link": "https://www.gskill.com/"
     },
     {
+        "name": "Intel",
+        "logo": "intel.svg",
+        "link": "https://www.intel.com/"
+    },
+    {
+        "name": "COLER MASTER",
+        "logo": "cooler_master.svg",
+        "link": "https://www.coolermaster.com/"
+    },
+    {
+        "name": "ASUS",
+        "logo": "asus.svg",
+        "link": "https://www.asus.com/"
+    },
+    {
+        "name": "ASRock",
+        "logo": "asrock.svg",
+        "link": "https://www.asrock.com/"
+    },
+    {
+        "name": "ROG",
+        "logo": "rog.svg",
+        "link": "https://rog.asus.com/"
+    },
+    {
+        "name": "Nvidia",
+        "logo": "nvidia.svg",
+        "link": "https://www.nvidia.com/"
+    },
+    {
+        "name": "Apacer",
+        "logo": "apacer.svg",
+        "link": "https://www.apacer.com/"
+    },
+    {
+        "name": "HIPER",
+        "logo": "hiper_power.svg",
+        "link": "https://hiper-power.com/"
+    },
+    {
+        "name": "SAPHHIRE",
+        "logo": "sapphire.svg",
+        "link": "https://www.sapphiretech.com/"
+    },
+    {
+        "name": "GOOD RAM",
+        "logo": "goodram.svg",
+        "link": "https://www.goodram.com/"
+    },
+    {
+        "name": "",
+        "logo": "",
+        "link": ""
+    },
+    {
+        "name": "",
+        "logo": "",
+        "link": ""
+    },
+    {
+        "name": "",
+        "logo": "",
+        "link": ""
+    },
+    {
+        "name": "",
+        "logo": "",
+        "link": ""
+    },
+    {
         "name": "",
         "logo": "",
         "link": ""
