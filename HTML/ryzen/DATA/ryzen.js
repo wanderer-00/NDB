a9055f533b3d053dbc973235726d3b67968294ef
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2023-02-28 02:41:14 +0300

    1

---
+++ HTML/ryzen/DATA/ryzen.js
@@ -0,0 +1,243 @@
+const ryzen = [
+	{
+		"name": "3 1200",
+		"score": 3539,
+		"price": 2000
+	},
+	{
+		"name": "3 1200 PRO",
+		"score": 3797,
+		"price": 
+	},
+	{
+		"name": "3 1300 X",
+		"score": 3921,
+		"price": 
+	},
+	{
+		"name": "3 1300 PRO",
+		"score": 4180,
+		"price": 
+	},
+	{
+		"name": "3 2100 PRO GE",
+		"score": 2521,
+		"price": 
+	},
+	{
+		"name": "3 2200 G PRO",
+		"score": 4068,
+		"price": 
+	},
+	{
+		"name": "3 2200 G",
+		"score": 3878,
+		"price": 
+	},
+	{
+		"name": "3 2200 GE",
+		"score": 3552,
+		"price": 
+	},
+	{
+		"name": "3 2200 GE PRO",
+		"score": 3536,
+		"price": 
+	},
+	{
+		"name": "3 2200 U",
+		"score": 2183,
+		"price": 
+	},
+	{
+		"name": "3 2300 U",
+		"score": 3066,
+		"price": 
+	},
+	{
+		"name": "3 2300 U PRO",
+		"score": 3599,
+		"price": 
+	},
+	{
+		"name": "3 2300 X",
+		"score": 4467,
+		"price": 
+	},
+	{
+		"name": "3 3100",
+		"score": 6465,
+		"price": 
+	},
+	{
+		"name": "3 3200 G",
+		"score": 4225,
+		"price": 
+	},
+	{
+		"name": "3 3200 G PRO",
+		"score": 4205,
+		"price": 
+	},
+	{
+		"name": "3 3200 GE",
+		"score": 4128,
+		"price": 
+	},
+	{
+		"name": "3 3200 GE PRO",
+		"score": 4008,
+		"price": 
+	},
+	{
+		"name": "3 3200 U",
+		"score": 2257,
+		"price": 
+	},
+	{
+		"name": "3 3250 C",
+		"score": 1833,
+		"price": 
+	},
+	{
+		"name": "3 3250 U",
+		"score": 2214,
+		"price": 
+	},
+	{
+		"name": "3 3300 U",
+		"score": 3283,
+		"price": 
+	},
+	{
+		"name": "3 3300U PRO",
+		"score": 3500,
+		"price": 
+	},
+	{
+		"name": "3 3300 X",
+		"score": 7124,
+		"price": 
+	},
+	{
+		"name": "3 3350 U",
+		"score": 3548,
+		"price": 
+	},
+	{
+		"name": "3 4100",
+		"score": 6701,
+		"price": 
+	},
+	{
+		"name": "3 4200 G PRO",
+		"score": 6815,
+		"price": 
+	},
+	{
+		"name": "3 4200 GE PRO",
+		"score": 6583,
+		"price": 
+	},
+	{
+		"name": "3 4300 G",
+		"score": 6611,
+		"price": 
+	},
+	{
+		"name": "3 4300 GE",
+		"score": 6754,
+		"price": 
+	},
+	{
+		"name": "3 4300 U",
+		"score": 4532,
+		"price": 
+	},
+	{
+		"name": "3 4350 G PRO",
+		"score": 6518,
+		"price": 
+	},
+	{
+		"name": "3 4350 GE PRO",
+		"score": 6673,
+		"price": 
+	},
+	{
+		"name": "3 4450 U PRO",
+		"score": 6063,
+		"price": 
+	},
+	{
+		"name": "3 5300 U",
+		"score": 5997,
+		"price": 
+	},
+	{
+		"name": "3 5300 G",
+		"score": 7845,
+		"price": 
+	},
+	{
+		"name": "3 5300 GE",
+		"score": 8213,
+		"price": 
+	},
+	{
+		"name": "3 5350 G PRO",
+		"score": 8370,
+		"price": 
+	},
+	{
+		"name": "3 5350 GE PRO",
+		"score": 8242,
+		"price": 
+	},
+	{
+		"name": "3 5400 U",
+		"score": 6393,
+		"price": 
+	},
+	{
+		"name": "3 5425 U",
+		"score": 7066,
+		"price": 
+	},
+	{
+		"name": "3 5450 U PRO",
+		"score": 6791,
+		"price": 
+	},
+	{
+		"name": "3 5475 U PRO",
+		"score": 6829,
+		"price": 
+	},
+	{
+		"name": "",
+		"score": ,
+		"price": 
+	},
+	{
+		"name": "",
+		"score": ,
+		"price": 
+	},
+	{
+		"name": "",
+		"score": ,
+		"price": 
+	},
+	{
+		"name": "",
+		"score": ,
+		"price": 
+	},
+	{
+		"name": "",
+		"score": ,
+		"price": 
+	},
+
+]
