9d2a8b7eb1c322dab221a1633f256cc93bc9ce7b
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2023-11-28 23:48:43 +0300

    -

---
+++ NEW/DATA/CPU.js
@@ -0,0 +1,73 @@
+var CPU = [
+    {
+        "VENDER":"AMD",
+        "LINE":"APU",
+        "FAMILY":"",
+        "GENERATION":"",
+        "NUMBER":"320",
+        "ABC SUFFIX":"FirePro"
+    },
+
+    {
+        "VENDER":"",
+        "LINE":"",
+        "FAMILY":"",
+        "GENERATION":"",
+        "NUMBER":"",
+        "ABC SUFFIX":""
+    },
+
+    {
+        "VENDER":"",
+        "LINE":"",
+        "FAMILY":"",
+        "GENERATION":"",
+        "NUMBER":"",
+        "ABC SUFFIX":""
+    },
+
+    {
+        "VENDER":"",
+        "LINE":"",
+        "FAMILY":"",
+        "GENERATION":"",
+        "NUMBER":"",
+        "ABC SUFFIX":""
+    },
+
+    {
+        "VENDER":"",
+        "LINE":"",
+        "FAMILY":"",
+        "GENERATION":"",
+        "NUMBER":"",
+        "ABC SUFFIX":""
+    },
+
+    {
+        "VENDER":"",
+        "LINE":"",
+        "FAMILY":"",
+        "GENERATION":"",
+        "NUMBER":"",
+        "ABC SUFFIX":""
+    },
+
+    {
+        "VENDER":"",
+        "LINE":"",
+        "FAMILY":"",
+        "GENERATION":"",
+        "NUMBER":"",
+        "ABC SUFFIX":""
+    },
+
+    {
+        "VENDER":"",
+        "LINE":"",
+        "FAMILY":"",
+        "GENERATION":"",
+        "NUMBER":"",
+        "ABC SUFFIX":""
+    }
+]
